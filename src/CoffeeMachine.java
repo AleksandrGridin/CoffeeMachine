12782f4522132fa7ba39763f4efb92ca4ce17848
Aleksandr Gridin <aleksandr.n.gridin@gmail.com>
2021-01-14 18:52:08 +0300

    Completed stage 2

--- src/CoffeeMachine.java
+++ src/CoffeeMachine.java
@@ -1,5 +1,23 @@
+import java.util.Scanner;
+
 public class CoffeeMachine {
+    private static Scanner scanner = new Scanner(System.in);
+
     public static void main(String[] args) {
+        getNumberIngredients();
+    }
+
+    private static void getNumberIngredients() {
+        System.out.println("Write how many cups of coffee you will need:");
+        int cups = scanner.nextInt();
+        System.out.printf("For %d cups of coffee you will need:\n", cups);
+        System.out.println(cups * 200 + " ml of water");
+        System.out.println(cups * 50 + " ml of milk");
+        System.out.println(cups * 15 + " g of coffee beans");
+        scanner.close();
+    }
+
+    private static void menu() {
         System.out.println("Starting to make a coffee");
         System.out.println("Grinding coffee beans");
         System.out.println("Boiling water");
